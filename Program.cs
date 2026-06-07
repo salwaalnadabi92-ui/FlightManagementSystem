4596cffd94f80b428d3f6ba01eefb1c7a58400ae
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-07 08:41:37 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿namespace MiniFlightManagementSystem
+{
+    internal class Program
+    {
+
+
+       
+
+
+
+
+
+
+
+static void Main(string[] args)
+        {
+
+
+            bool exit = false;
+
+            while (exit == false)
+            {
+
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
+                Console.WriteLine(" ___________________________________");
+                Console.WriteLine("1.  ");
+                Console.WriteLine("2. ");
+                Console.WriteLine("3. ");
+                Console.WriteLine("4. ");
+                Console.WriteLine("5. ");
+                Console.WriteLine("6. ");
+                Console.WriteLine("7.");
+                Console.WriteLine("8. ");
+                Console.WriteLine("9. ");
+                Console.WriteLine("10. ");
+                Console.WriteLine("------------------------------------");
+
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+
+                    case 1:
+
+
+
+                        break;
+
+                    case 2:
+
+
+                        break;
+
+                    case 3:
+
+
+                        break;
+
+                    case 4:
+
+
+
+                        break;
+
+                    case 5:
+
+
+                        break;
+
+                    case 6:
+
+
+
+                        break;
+
+                    case 7:
+
+
+
+
+                        break;
+                    case 8:
+
+                        break;
+
+                    case 9:
+
+                        break;
+
+                    case 10:
+
+                        break;
+
+                    default:
+
+                        break;
+
+                }//switch
+
+
+                Console.WriteLine("Enter any key");
+
+                Console.ReadKey();
+                Console.Clear();
+
+
+            }//while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
